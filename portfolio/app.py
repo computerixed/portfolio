701fb65e5b0ba96a27d13db74125ff5915abef17
Computerixed <76139107+computerixed@users.noreply.github.com>
2022-08-27 00:30:31 +0100

    Update app.py
    Changed for deployment

--- portfolio/app.py
+++ portfolio/app.py
@@ -16,5 +16,5 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__:
+    app.run(host='127.0.0.1',port=3000,debug=True)
